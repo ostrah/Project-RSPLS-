a78e9f22809e1636d01af559f1e997a386533130
mediatorvvsh <mediatorvvsh@gmail.com>
2023-09-28 23:59:11 +0300

    FibonacciRecursion

---
+++ FibonacciRecursion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FibonacciRecursion
+{
+    class Program
+    {
+        static int Fibonacci(int number)
+        {
+            if (number < 2)
+            {
+                return number;
+            }
+            else
+            {
+                return Fibonacci(number - 1) + Fibonacci(number - 2);
+            }
+        }
+
+        static void Main()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                int fibonacci = Fibonacci(i);
+                Console.Write($"{fibonacci}, ");
+            }
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
